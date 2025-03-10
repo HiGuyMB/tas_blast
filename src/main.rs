891164fb214a0ad6f65182d6940263970694e3fc
HiGuy Smith <higuymb@gmail.com>
2025-03-09 23:42:00 -0400

    Better test drivers

--- src/main.rs
+++ src/main.rs
@@ -16,25 +16,25 @@ fn main() -> Result<()> {
     match args.get(1).ok_or(USAGE_ERROR)?.as_str() {
         "--import" => {
             let conts =
-                fs::read(args.get(2).ok_or(usage_error)?)?;
+                fs::read(args.get(2).ok_or(USAGE_ERROR)?)?;
             let mut bs = BitStream::new(conts);
 
             let r = Recording::from_stream(&mut bs)?;
             let tf = TasFile::from_rec(r);
             let mut v: Vec<u8> = vec![];
             tf.print(&mut v)?;
-            fs::write(args.get(3).ok_or(usage_error)?, v)?;
+            fs::write(args.get(3).ok_or(USAGE_ERROR)?, v)?;
             Ok(())
         }
         "--export" => {
-            let input = fs::read_to_string(args.get(2).ok_or(usage_error)?)?;
+            let input = fs::read_to_string(args.get(2).ok_or(USAGE_ERROR)?)?;
             let tf = TasFile::parse(input)?;
             println!("Read {} seqs", tf.sequences.len());
             let r = tf.into_rec();
 
             let mut os = BitStream::new(vec![]);
             r.into_stream(&mut os)?;
-            fs::write(args.get(3).ok_or(usage_error)?, os.into_bytes())?;
+            fs::write(args.get(3).ok_or(USAGE_ERROR)?, os.into_bytes())?;
             Ok(())
         }
         "--test-rec" => {
@@ -56,21 +56,34 @@ fn main() -> Result<()> {
             let mut bs = BitStream::new(conts);
 
             let r = Recording::from_stream(&mut bs)?;
+            fs::write("rec1.json", serde_json::to_string_pretty(&r)?)?;
+            fs::write("rec1.rec", bs.bytes())?;
+
             let tf = TasFile::from_rec(r);
             println!("Read {} seqs", tf.sequences.len());
 
             let mut v: Vec<u8> = vec![];
             tf.print(&mut v)?;
+
+            println!("{}", String::from_utf8_lossy(&v));
+
             let tf2 = TasFile::parse(String::from_utf8(v)?)?;
             let r2 = tf2.into_rec();
+            fs::write("rec2.json", serde_json::to_string_pretty(&r2)?)?;
 
             let mut os = BitStream::new(vec![]);
             r2.into_stream(&mut os)?;
+            fs::write("rec2.rec", os.bytes())?;
+
+            os.seek(0, 0);
+            let r3 = Recording::from_stream(&mut os)?;
+            fs::write("rec3.json", serde_json::to_string_pretty(&r3)?)?;
+
             assert_eq!(bs.into_bytes(), os.into_bytes());
             Ok(())
         }
         "--test-rect" => {
-            let input = fs::read_to_string(args.get(2).ok_or(usage_error)?)
+            let input = fs::read_to_string(args.get(2).ok_or(USAGE_ERROR)?)
                 ?;
             let tf = TasFile::parse(input.clone())?;
             println!("Read {} seqs", tf.sequences.len());
@@ -87,6 +100,6 @@ fn main() -> Result<()> {
             assert_eq!(String::from_utf8(v)?, input);
             Ok(())
         }
-        _ => Err(GenericError(usage_error).into()),
+        _ => Err(GenericError(USAGE_ERROR).into()),
     }
 }
